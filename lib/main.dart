c0fc154371781f9518c5e4ac6879529c36eeeb19
Nancy Alaa <nancyalaa733@gmail.com>
2024-06-27 10:18:42 +0300

    my changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:challange_project_1/Screen/loginScreen.dart';
 import 'package:challange_project_1/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:challange_project_1/Screen/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:Loginscreen(),
+      home:Homepage(),
     );
   }
 }
